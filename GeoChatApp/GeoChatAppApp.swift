281e362b8c5cf6d97b4beaf5c1ea3711828be8e8
Shaz-M <shazmaradya@yahoo.com>
2022-07-24 19:13:10 -0500

    Initial Commit

---
+++ GeoChatApp/GeoChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoChatAppApp.swift
+//  GeoChatApp
+//
+//  Created by Shaz on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct GeoChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
